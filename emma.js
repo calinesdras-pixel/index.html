4ceb16a720ac1d4c2cb964fdf357dbcce2dc8878
CALESS <calinesdras@gmail.com>
2026-04-20 18:48:24 +0200

    Add files via upload

---
+++ emma.js
@@ -0,0 +1,44 @@
+alert("J ai fait de mon mieux pour réaliser ce site  , j espère qu il te plaira...")
+// console.log(5+9)
+// console.log(5-9)
+// console.log(5*9)
+// console.log(5/9)
+// console.log(5**9)
+// console.log("string".toUpperCase())
+// const name = "Esdras"
+// let currentpage="emma"
+// let number = 0
+// number=3
+// currentpage="popoh"
+// console.log(name+currentpage+number)
+// const a = 10
+// const b = 5 
+// console.log(b<a)
+// console.log(b>a)
+// console.log(b<=a)
+// console.log(b>=a)
+// console.log(b!=a)
+// console.log(b==a)
+// console.log(b===a)
+// console.log(b!==a)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+// console.log(false || false)
+// console.log(false || true)
+// console.log(true || false)
+// console.log(true || true
+
+// )
+
+
+
+
+
+
+
+
+
+
+
